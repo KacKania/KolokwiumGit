72475c66a186261cb633b1c75bc8d97c5c28185e
KacKania <72346717+KacKania@users.noreply.github.com>
2023-03-23 21:06:40 +0100

    lab3

--- LabPK.cpp
+++ LabPK.cpp
@@ -1,66 +1,74 @@
-﻿
-#include "Funckje.h"
+﻿#include <iostream>
 
+/* Zmieniając ostatnią literę w poniższej instrukcji na D (dodatkowe), lub P (podstawowe)
+*  wybierasz aktywne zadanie.
+*
+*  UWAGA: jeśli rozwiązanie zadań wymaga podziału programu na pliki to dodaj je wszystkie do
+*  projektu, ale przy każdym zadaniu "includuj" tylko te, które jego dotyczą.
+*
+*/
+#define ZadanieD 
 
+#ifdef ZadanieD
+
+// TU UMIEŚĆ KOD ROZWIAZANIA WYBRANEGO ZADANIA DOMOWEGO 
+
+void zwolnienie_pamieci(int*** tab2D, int wiersze)
+{
+	bool rodzaj;			//true ciagla	false fragmentaryczna
+
+	if (**tab2D != nullptr)
+	{
+		for (int i = 0; i < wiersze; i++)
+		{
+			if (tab2D[i][0] < tab2D[i + 1][0])
+			{
+				rodzaj = true;
+			}
+			else
+			{
+				rodzaj = false;
+				break;
+			}
+		}
+
+		if (rodzaj)
+		{
+			delete[] tab2D[0];
+			delete[] * *tab2D;
+		}
+		else
+		{
+			for (int i = 0; i < wiersze; i++)
+			{
+				delete[] tab2D[i];
+			}
+			delete[] * *tab2D;
+		}
+		tab2D = nullptr;
+	}
+
+	//sprawdzanie rozmiaru
+
+	/*
+	for (int rozmiar : tab2D)
+	{
+		int* tab = new int[rozmiar] {};
+		unsigned char* wsk = (unsigned char*)tab;
+	}
+	*/
+ }
+#endif
 
 #ifdef ZadanieP
 
 // TU UMIEŚĆ KOD ROZWIAZANIA ZADANIA PODSTAWOWEGO
-int main()
-{
-	int tab[10][kolumny] = { {4,3,6,1,2,5,6,7,9,5},
-							{3,6,3,8,9,1,4,5,6,9},
-							{2,6,6,4,8,2,6,3,3,7},
-							{6,3,7,2,8,1,4,5,3,6},
-							{8,2,1,4,6,3,7,2,5,7},
-							{4,3,6,1,2,5,6,7,9,5},
-							{3,6,3,8,9,1,4,5,6,9},
-							{2,6,6,4,8,2,6,3,3,7},
-							{6,3,7,2,8,1,4,5,3,6},
-							{8,2,1,4,6,3,7,2,5,7},
-												};
 
-	int tab2[6][kolumny] = {{4,3,6,1,2,5,6,7,9,5},
-							{3,6,3,8,9,1,4,5,6,9},
-							{2,6,6,4,8,2,6,3,3,7},
-							{6,3,7,2,8,1,4,5,3,6},
-							{8,2,1,4,6,3,7,2,5,7},
-							{5,3,4,1,6,7,8,9,0,1},
-												};
-
-	cout << "Nieposortowana: " << endl;
-	wypisz(tab,10);
-	Sortuj(tab);
-	cout << "Posortowana: "<< endl;
-	wypisz(tab, 10);
-
-	cout << "Nieposortowana: " << endl;
-	wypisz(tab2,6);
-	Sortuj(tab2,6);
-	cout << "Posortowana: " << endl;
-	
-	wypisz(tab2,6);
-
-}
 #endif
 
 #ifdef ZadanieA
 
 // TU UMIEŚĆ KOD ROZWIAZANIA ZADANIA AMBITNEGO
-enum class Stany
-{
-	Stan1 = 0,
-	Stan2 = 1,
-	Stan3 = 2,
-	Stan4 = 3
-};
-
-struct DaneKom
-{
-	bool dzialaj;
-	Stany aktStan;
-};
-
 
 #endif
 
