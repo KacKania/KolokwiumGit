cdc0dad2adec0313a02999e09dbbdba288fb4130
KacKania <72346717+KacKania@users.noreply.github.com>
2023-04-27 19:36:38 +0200

    L_2.1

--- LabPK.cpp
+++ LabPK.cpp
@@ -1,144 +1,140 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <cstdio>
-#include <fstream>
-#include <stdio.h>
-#include <string>
-#include <cstring>
-using namespace std;
+﻿#include <iostream>
+#include <ctime>
+#include <cstdlib>
 
-#define ZadanieD
+using namespace std;
+/* Zmieniając ostatnią literę w poniższej instrukcji na D (dodatkowe), lub P (podstawowe)
+*  wybierasz aktywne zadanie.
+*
+*  UWAGA: jeśli rozwiązanie zadań wymaga podziału programu na pliki to dodaj je wszystkie do
+*  projektu, ale przy każdym zadaniu "includuj" tylko te, które jego dotyczą.
+*
+*/
+#define ZadanieD 
 
 #ifdef ZadanieD
-int ileWierszy(FILE* plik)
-{
-	int ilosc = 0;
-	char tekst[100] = {};
 
-	while (fscanf(plik, "%[^\n] ", &tekst) != EOF)
-	{
-		ilosc++;
-	}
-	return ilosc;
-}
-ostream& operator<< (ostream& ekr, FILE* plik)
+// TU UMIEŚĆ KOD ROZWIAZANIA WYBRANEGO ZADANIA DOMOWEGO 
+
+#define RND_ZZ (double)rand() / RAND_MAX
+#define RND_ZO (double)rand() / (RAND_MAX + 1)
+#define RND_OZ (rand() + 1.0) / (RAND_MAX + 1)
+#define RND_OO (rand() + 1.0) / (RAND_MAX + 2)
+
+class Generator
 {
-	char znak;
-	if (plik != nullptr)
+	double m_a;
+	double m_b;
+	bool m_czyLewaGranicaOtwarta;
+	bool m_czyPrawaGranicaOtwarta;
+	double m_losowania[4];
+	int m_index = 0;
+	int m_ziarno;
+	double losuj();
+	bool sprawdzPowtorzenie(double x);
+public:
+
+	void setAll(double a, double b, bool lewaGranica, bool prawaGranica, int ziarno)
 	{
-		rewind(plik);
-		while (!feof(plik))
-		{
-			znak = fgetc(plik);
-			ekr << znak;
-		}
+		setA(a);
+		setB(b);
+		setLewaGranica(lewaGranica);
+		setPrawaGranica(prawaGranica);
+		setZiarno(ziarno);
 	}
-	return ekr;
+	void setA(double a) { m_a = a; }
+	void setB(double b) { m_b = b; }
+	void setLewaGranica(bool granica) { m_czyLewaGranicaOtwarta = granica; }
+	void setPrawaGranica(bool granica) { m_czyPrawaGranicaOtwarta = granica; }
+	void setZiarno(int ziarno){ m_ziarno = ziarno; }
+
+	double generujLosowa();
+};
+
+double Generator::losuj()
+{
+	double wylosowana;
+
+	if (!m_czyLewaGranicaOtwarta && !m_czyPrawaGranicaOtwarta)
+	{
+		wylosowana = m_a + RND_ZZ * (m_b - m_a);
+	}
+	else if (!m_czyLewaGranicaOtwarta && m_czyPrawaGranicaOtwarta)
+	{
+		wylosowana = m_a + RND_ZO * (m_b - m_a);
+	}
+	else if (m_czyLewaGranicaOtwarta && !m_czyPrawaGranicaOtwarta)
+	{
+		wylosowana = m_a + RND_OZ * (m_b - m_a);
+	}
+	else if (m_czyLewaGranicaOtwarta && m_czyPrawaGranicaOtwarta)
+	{
+		wylosowana = m_a + RND_OO * (m_b - m_a);
+	}
+
+	return wylosowana;
 }
+
+bool Generator::sprawdzPowtorzenie(double x)
+{
+	bool sprawdz;
+	for (int i = 0; i < 4; i++)
+	{
+		if (x != m_losowania[i])
+			sprawdz = true;
+		else
+			sprawdz = false;
+	}
+
+	return sprawdz;
+}
+
+double Generator::generujLosowa()
+{
+	double los = losuj();
+
+	if (sprawdzPowtorzenie(los))
+	{
+		m_losowania[m_index] = los;
+		m_index++;
+		if (m_index > 3)
+			m_index = 0;
+		return los;
+	}
+	else
+		generujLosowa();
+
+}
+
 int main()
 {
-	setlocale(LC_ALL, "");
-	constexpr size_t ROZMIAR = 100;
-	FILE* tdostawa = nullptr;
-	FILE* todleglosc = nullptr;
-	todleglosc = fopen("taryfa_odleglosc.txt", "r");
-	tdostawa = fopen("taryfa_dostawa.txt", "r");
-	int rodleglosc = ileWierszy(todleglosc);
-	int rdostawa = ileWierszy(tdostawa);
+	srand(time(0));
 
-	double* odleglosc_km = new double[rodleglosc];
-	double* odleglosc_cena = new double[rodleglosc];
-	double* dostawa_cena = new double[rdostawa];
+	Generator g;
+	g.setAll(2.5, 2.6, 1, 1, time(0));
 
-	rewind(todleglosc);
-	for (int i = 0; i < rodleglosc; i++)
-	{
-		fscanf(todleglosc, "%d", odleglosc_km);
-
-	}
-	
-	
-	rewind(tdostawa);
-	for (int i = 0; i < rdostawa; i++)
-	{
-		fscanf(tdostawa, "%d", dostawa_cena);
-	}
-
-	fclose(todleglosc);
-	todleglosc = nullptr;
-
-
-	double odleglosc;
-	int ster_dostawa;
-	double taryfa_odleglosc = 0;
-	double taryfa_dostawa = 0;
-	double cena = 0;
-	cout << "Podaj odleglosc " << endl;
-	cin >> odleglosc;
-	cout << " Wybierz sposob dostawy podajac [nr indeksu]: " << endl;
-	cout << tdostawa << endl;
-	cin >> ster_dostawa;
-
-	fclose(tdostawa);
-	tdostawa = nullptr;
-
-	for (int i = 0; i < rodleglosc; i++)
-	{
-		if (odleglosc <= 0)
-		{
-			cout << "za mala odleglosc";
-			break;
-		}
-		
-		if (odleglosc <= odleglosc_km[i])
-		{
-			taryfa_odleglosc = odleglosc_cena[i];
-			break;
-		}
-	}
-	for (int i = 0; i <= ster_dostawa; i++)
-	{
-		if (ster_dostawa<0)
-		{
-			cout << "Zly indeks!"; break;
-		}
-		else
-		{
-			taryfa_dostawa = dostawa_cena[i]; 
-		}
-			
-	}
-
-	cena = taryfa_dostawa * taryfa_odleglosc;
-
-
-	cout << "Cena: " << cena;
-
-
-
-
-
+	cout << g.generujLosowa() << endl;
+	cout << g.generujLosowa() << endl;
+	cout << g.generujLosowa() << endl;
+	cout << g.generujLosowa() << endl;
+	cout << g.generujLosowa() << endl;
+	cout << g.generujLosowa() << endl;
 }
-
-#endif
-
-#ifdef ZadanieW
-
-
 
 #endif
 
 #ifdef ZadanieP
 
-
+// TU UMIEŚĆ KOD ROZWIAZANIA ZADANIA PODSTAWOWEGO
 
 #endif
 
 #ifdef ZadanieA
 
-
+// TU UMIEŚĆ KOD ROZWIAZANIA ZADANIA AMBITNEGO
 
 #endif
+
 
 
 
